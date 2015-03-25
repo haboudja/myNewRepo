d4c4c94c2b74b6367cc03d684a628d2a721c0599
Hyacinthe Aboudja <haboudja@okcu.edu>
2015-03-25 15:44:56 -0500

    This is a new commit of the fill sum

--- sum.c
+++ sum.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 main(){
-	int i,value =10,sum=0;
+	int i,value =100,sum=0;
 	for(i=0;i<= value;i++)
 		sum +=i;
 	printf(" The sum is : %d \n",sum);
+
+	printf(" This is a second version of sum ...\n");
 }
 
