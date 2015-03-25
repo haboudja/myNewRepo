503e6293f5f6c5ad7de9f5e61ded8d2bac4c8aee
Hyacinthe Aboudja <haboudja@okcu.edu>
2015-03-25 15:33:34 -0500

    This is a set of new files to be committed

---
+++ Addition.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+  int number1, number2, sum = 0;
+  printf(" Please enter the 2 numbers: ");
+  scanf("%d%d",&number1,&number2);
+  sum = number1 + number2;
+  printf(" The sum of the 2 numbers is: %d \n", sum);
+}
